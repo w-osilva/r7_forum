e6f532a10a0159b7cfe66f69bc2070431a74f666
Washington Silva <w-osilva@hotmail.com>
2016-02-07 03:35:15 -0200

    Adding blacklist

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,17 +1,12 @@
 module TopicsHelper
 
-  def show_nested_comments(topic_number, comments)
-    first_level_comments = comments.select {|c| c.is_first_level? }
-    get_html_nested_comments(topic_number, comments)
-  end
-
   def get_html_nested_comments(topic_number, comments)
     html = "<ul style='list-style-type:none'>"
     comments.each_with_index do |c, i|
       index = (c.is_first_level?) ? get_index_by_page + (i + 1) : (i + 1)
       index = "#{topic_number}.#{index}"
       html +="  <li><strong>#{index}</strong>
-                  #{c.created_at} #{c.text}
+                  #{c.created_at} #{Blacklist.sanitize c.text}
                   <a href='/topics/#{c.get_topic.id}/comments/#{c.id}/comments/new'>[Reply]</a>"
       html +=" #{get_html_nested_comments("#{index}", c.comments)}" unless c.comments.empty?
       html +="</li>"
